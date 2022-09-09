1ca5b85fa0a9bdbff1fba131cbc928d53c571517
DiabolusGX <ankit.malik1092@gmail.com>
2022-09-09 22:54:23 +0530

    update timeout

--- index.js
+++ index.js
@@ -126,12 +126,12 @@ setInterval(() => {
 
 	// story scheduler
 	if (dailyStory != day) {
-		// 1 min timeout
+		// 2 min timeout
 		// instagram rate limit on login on Friday
 		// login required for both post & story on same time on Friday
 		setTimeout(() => {
 			post("story");
-		}, 60000);
+		}, 2 * 60 * 1000);
 		dailyStory = day;
 		console.info(
 			`[${new Date().toISOString()}] [interval] Published stories.`
